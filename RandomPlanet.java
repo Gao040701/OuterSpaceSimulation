c51b48f5fcad8c2e784bf97cf68a5ffabd69c1d0
Gao040701 <349968818@gapps.yrdsb.ca>
2023-11-07 18:23:00 -0500

    organized images for planet, setImage for planet

--- RandomPlanet.java
+++ RandomPlanet.java
@@ -8,12 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class RandomPlanet extends Planet {
     private boolean canSpawnNext;
-
+    
+    //variables for set the image;
+    private GreenfootImage[] planets = new GreenfootImage[7];
+    private int planetImgIndex, length;
+    private GreenfootImage img;
     public RandomPlanet() {
         // 在构造函数中设置RandomPlanet的初始位置和速度
         setLocation(0, Greenfoot.getRandomNumber(276) + 150); // 设置y坐标在150到425之间随机
         speed=Greenfoot.getRandomNumber(3) + 1; // 设置x方向速度
         canSpawnNext = false;
+        
+        //set the greenfootImage array for random setImage
+        for (int i = 0; i < 7; i++){
+            planets[i] = new GreenfootImage("planets/planet" + i + ".png");
+        }
+        //set random img in random size 
+        randomImage();
     }
 
     public void checkCollision(){
@@ -34,5 +45,14 @@ public class RandomPlanet extends Planet {
             getWorld().addObject(new RandomPlanet(), 0, Greenfoot.getRandomNumber(276) + 150);
             canSpawnNext = false; // 防止连续生成多个RandomPlanet
         }
+    }
+    
+    private void randomImage(){
+        planetImgIndex = Greenfoot.getRandomNumber(7);
+        img = planets[planetImgIndex];
+        //scale from 200*200 to 240*240
+        length = 200+Greenfoot.getRandomNumber(5);
+        img.scale(length, length);
+        setImage(img);
     }
 }
