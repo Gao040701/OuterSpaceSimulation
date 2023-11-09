99617537748c162306813d46fedb0cee7b93d9a3
JenZ-00 <349941245@gapps.yrdsb.ca>
2023-11-09 12:08:06 -0500

    Added Prince and Fox' spawning & rotations

--- Fox.java
+++ Fox.java
@@ -8,12 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Fox extends Being
 {
+    private int speed;
+    private Planet touchingPlanet;
     /**
      * Act - do whatever the Fox wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        if (getOneIntersectingObject(Planet.class) != null){
+            touchingPlanet = (Planet)getOneIntersectingObject(Planet.class);
+            rotate(touchingPlanet.getRadius());
+        }
+    }
+
+    public void rotate(double planet){
+        move(planet);
+        turn(0.5);
     }
 }
--- Galaxy.java
+++ Galaxy.java
@@ -18,19 +18,32 @@ public class Galaxy extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 576, 1, false);
         prepare();
-        
-        addObject(new RandomPlanet(), 500, 250);
     }
-    
+
     private void prepare() {
         for (int i = 0; i < numOfAsteriods; i++) {
-            addObject(new Asteroids(), Greenfoot.getRandomNumber(getWidth()), Greenfoot.getRandomNumber(getHeight()));
+            addObject(new Asteroids(), getAsteroidsX(), getAsteroidsY());
         }
         addObject(new LittlePrince(), 100, 100);
-
+        if (Greenfoot.getRandomNumber (2) == 0){
+            addObject (new Fox(), 0 + 50, Greenfoot.getRandomNumber(276) + 200);
+        }
         HomePlanet homePlanet = new HomePlanet();
         addObject(homePlanet, getWidth() / 2, getHeight() / 2);
         addObject(new RandomPlanet(), 0, Greenfoot.getRandomNumber(276) + 150);
     }
+    
+    public int getAsteroidsX(){
+        int getAsteroidsX = Greenfoot.getRandomNumber(getWidth());
+        return getAsteroidsX;
+    }
+    
+    public int getAsteroidsY(){
+        int getAsteroidsY = Greenfoot.getRandomNumber(getHeight());
+        return getAsteroidsY;
+    }
 
+    public void act(){
+
+    }
 }
