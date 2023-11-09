00fbac9d6e7870c0d80c4fdde294aba6033c9604
JenZ-00 <349941245@gapps.yrdsb.ca>
2023-11-09 10:56:44 -0500

    Added fox

---
+++ Character.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Character here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Character extends SuperSmoothMover
+{
+    /**
+     * Act - do whatever the Character wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- Fox.java
+++ Fox.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class test here.
+ * Write a description of class Fox here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class test extends Actor
+public class Fox extends Character
 {
     /**
-     * Act - do whatever the test wants to do. This method is called whenever
+     * Act - do whatever the Fox wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
