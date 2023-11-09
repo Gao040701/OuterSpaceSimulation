cae6ea2719bbb7325f4611c8ce2a95d1c94b90ef
JenZ-00 <349941245@gapps.yrdsb.ca>
2023-11-09 11:06:56 -0500

    Changed MyWorld to Galaxy

--- Galaxy.java
+++ Galaxy.java
@@ -5,7 +5,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Galaxy extends World
 {
     private int numOfAsteriods=3;
     private int numOfPlanets = 1; 
@@ -13,7 +13,7 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public Galaxy()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 576, 1, false);
