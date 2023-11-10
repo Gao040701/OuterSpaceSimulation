dfa041b5060ee33da0107a1cb1d642aa6e2707e0
Gao040701 <349968818@gapps.yrdsb.ca>
2023-11-10 12:10:34 -0500

    add EndScreen, TitleScreen, Rose, and SetValuePae

---
+++ Rose.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rose here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rose extends Stationary
+{
+    /**
+     * Act - do whatever the Rose wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if (isTouching(LittlePrince.class)){
+            Greenfoot.setWorld(new EndScreen());
+        }
+    }
+}
