ff516aa45b45f96cd30b60e2e381b55c16c45568
JenZ-00 <349941245@gapps.yrdsb.ca>
2023-11-11 11:08:28 -0500

    Fixed bugs in the random mvts for Moving class

--- Moving.java
+++ Moving.java
@@ -22,18 +22,88 @@ public class Moving extends Being
      */
     public void act()
     {
-        if (targetPlanet != null && targetPlanet.getWorld() == null){
+        /**
+         * Add if (energy > 0)
+         */
+        if (targetPlanet != null){ //&& targetPlanet.getWorld() == null
             targetPlanet = null;
-        }
-        
-        if (targetPlanet != null){
             moveTowardPlanet();
+            System.out.println("Touched!??");
         }
         else{
             moveRandomly();
         }
     }
 
+    private void moveTowardPlanet(){
+        if (Galaxy.getDistance(this, targetPlanet) < 10){
+            rotate();
+            System.out.println("Touched the planet!");
+        }
+        else{
+            move (mySpeed);
+        }
+    }
+
+    private void targetClosestPlanet(){
+        double closestTargetDistance = 0;
+        double distanceToActor;
+        planets = (ArrayList<Planet>)getObjectsInRange(40, Planet.class);
+        if (planets.size() == 0){
+            planets = (ArrayList<Planet>)getObjectsInRange(140, Planet.class);
+        }
+
+        if (planets.size() > 0){
+            targetPlanet = planets.get(0);
+            closestTargetDistance = Galaxy.getDistance (this, targetPlanet);
+
+            for (Planet o : planets){
+                distanceToActor = Galaxy.getDistance(this, o);
+
+                if (distanceToActor < closestTargetDistance){
+                    targetPlanet = o;
+                    closestTargetDistance = distanceToActor;
+                }
+            }
+            turnTowards(targetPlanet.getX(), targetPlanet.getY());
+        }
+    }
+
+    private void moveRandomly(){
+        if((getX() > 30 || getX() < 970) && (getY() > 30 || getY() < 540)){
+            if (Greenfoot.getRandomNumber (100) == 50){
+                turn (Greenfoot.getRandomNumber(360));
+            }
+            else{
+                move (mySpeed);
+            }
+        }
+        if (getX() < 30 ){
+            setLocation(getX() + 40, getY());
+            turn (Greenfoot.getRandomNumber(1));
+            move (mySpeed);
+            System.out.println("move right");
+        }
+        if (getX() > 970 ){
+            setLocation(getX() - 10, getY());
+            turn (Greenfoot.getRandomNumber(180));
+            move (mySpeed);
+            System.out.println("move left");
+        }
+        if (getY() < 30 ){
+            setLocation(getX(), getY() + 10);
+            turn (Greenfoot.getRandomNumber(270));
+            move (mySpeed);
+            System.out.println("move down");
+        }
+        if (getY() >540 ){
+            setLocation(getX(), getY() - 10);
+            turn (Greenfoot.getRandomNumber(90));
+            move (mySpeed);
+            System.out.println("move up");
+        }
+    }
+    
     public void rotateDetection(){
         if (getOneIntersectingObject(Planet.class) != null){
             rotateDetection = true;
@@ -45,48 +115,6 @@ public class Moving extends Being
             move(touchingPlanet.getSpeed());
         }
         //rotate(119/100);
-    }
-    
-    private void moveTowardPlanet(){
-        if (Galaxy.getDistance(this, targetPlanet) < 18){
-            rotate();
-        }
-        else{
-            move (mySpeed);
-        }
-    }
-    
-    private void targetClosestPlanet(){
-        double closestTargetDistance = 0;
-        double distanceToActor;
-        planets = (ArrayList<Planet>)getObjectsInRange(40, Planet.class);
-        if (planets.size() == 0){
-            planets = (ArrayList<Planet>)getObjectsInRange(140, Planet.class);
-        }
-        
-        if (planets.size() > 0){
-            targetPlanet = planets.get(0);
-            closestTargetDistance = Galaxy.getDistance (this, targetPlanet);
-            
-            for (Planet o : planets){
-                distanceToActor = Galaxy.getDistance(this, o);
-                
-                if (distanceToActor < closestTargetDistance){
-                    targetPlanet = o;
-                    closestTargetDistance = distanceToActor;
-                }
-            }
-            turnTowards(targetPlanet.getX(), targetPlanet.getY());
-        }
-    }
-    
-    private void moveRandomly(){
-        if (Greenfoot.getRandomNumber (100) == 50){
-            turn (Greenfoot.getRandomNumber(360));
-        }
-        else{
-            move (mySpeed);
-        }
     }
 
     public void rotate(){
