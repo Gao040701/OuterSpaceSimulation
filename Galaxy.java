583dadb1fadfe3d83e404cadd27fecbe0e7af228
JenZ-00 <349941245@gapps.yrdsb.ca>
2023-11-11 20:30:41 -0500

    Created setPaintOrder

--- Galaxy.java
+++ Galaxy.java
@@ -20,6 +20,7 @@ public class Galaxy extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 576, 1, false);
+        setPaintOrder (SuperStatBar.class, Asteroids.class, Being.class, Planet.class); 
         clueBar = new SuperStatBar(110, clueCount, null, 110, 10, 0, Color.RED, Color.BLACK, false, Color.BLACK, 1);
         addObject(clueBar, getWidth() - 60, 80);
         prepare();
