c349d4aca3718085251fb47564e3c2d5cb7553eb
joyceylii <149703878+joyceylii@users.noreply.github.com>
2023-11-26 23:57:32 -0500

    API

--- Rose.java
+++ Rose.java
@@ -1,22 +1,31 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Rose here.
- * 
- * @author Angela Gao, Jiayi Li 
- * @version November 2023
+/** The Rose class represents fixed objects in the game, specifically roses.
+  * Roses are decorative elements and do not interact with other objects in the game.
+  *The purpose of simulation is to let the little prince find the rose. When the clue bar is full, the rose will appear on the planet.
+  *
+  * Usage:
+  * <li>Create Rose instances in the game world for decoration purposes. </li>
+  * <li>The rose is stationary and does not perform any specific action. </li>
+  * 
+  * @author Angela Gao, Jiayi Li 
+  * @version November 2023
  */
 public class Rose extends Stationary
 {
     private GreenfootImage rose = new GreenfootImage("rose.png");
     /**
-     * Act - do whatever the Rose wants to do. This method is called whenever
+     * Act - rose moves on planet with the speed of one
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         move(1);
     }
+    /**
+     * Constructor for the Rose class.
+     * Sets the image for the rose using the provided GreenfootImage.
+     */
     public Rose(){
         setImage(rose);
     }
